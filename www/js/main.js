f0fdad3020fcbde2f3323c82aa60ea48aa8fa51f
Gui O'Connor <guioconnor@gmail.com>
2014-05-25 10:55:51 +0100

    Sticky header across the website

---
+++ www/js/main.js
@@ -0,0 +1,10 @@
+(function($) {
+
+    $(document).ready(function(){
+        $("#main-menu").sticky({topSpacing:0});
+        $("#branding").sticky({topSpacing:-150});
+        $("#article").sticky({topSpacing:38});
+        $("#header").sticky({topSpacing:-45});
+    });
+
+})(jQuery);
