88bc4d3f69c26750e7b26d7a0d83e68bbed97dc6
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2016-02-14 01:38:54 +0000

    Made site responsive

---
+++ www/js/scripts.js
@@ -0,0 +1,13 @@
+(function init() {
+	var hamburger = document.getElementById('hamburger');
+	var body = document.body;
+
+	hamburger.addEventListener('click', function(){
+		if( body.getAttribute('class') == 'hamburger-active') {
+			body.setAttribute('class', '');
+		}
+		else {
+			body.setAttribute('class', 'hamburger-active');
+		}
+	})
+})()
