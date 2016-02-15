dfc214aece9a11e6d2ab3b0a9e6e67e78ebafea4
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2016-02-15 23:36:59 +0000

    Hamburger menu on home page

--- www/js/main.js
+++ www/js/main.js
@@ -1,10 +1,30 @@
 (function($) {
 
     $(document).ready(function(){
+    	/*
+    	 *  Sticky header
+    	 */
         $("#main-menu").sticky({topSpacing:0});
         $("#branding").sticky({topSpacing:-150});
         $("#article").sticky({topSpacing:38});
         $("#header").sticky({topSpacing:-45});
+
+        /*
+         *  Hamburger menu
+         */
+    	var hamburger = document.getElementById('hamburger');
+		var body = document.body;
+
+		hamburger.addEventListener('click', function(){
+			if( body.getAttribute('class') == 'hamburger-active') {
+				body.setAttribute('class', '');
+			}
+			else {
+				body.setAttribute('class', 'hamburger-active');
+			}
+		});
+
     });
 
 })(jQuery);
+
