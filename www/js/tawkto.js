690127f1e6a9355f2cb0daa03de4877ed147c858
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2016-08-29 23:46:47 +0100

    first take at integrating tawk.to

---
+++ www/js/tawkto.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/57c4b7720e2ec4134ce350eb/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
