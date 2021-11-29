3c7bc829cfdfac9bcc94235b2fe76af051f3e7e3
Guilherme Zühlke O'Connor <guioconnor@guioconnor.com>
2021-11-29 23:59:48 +0000

    Mock param

--- js/countdown.js
+++ js/countdown.js
@@ -2,8 +2,15 @@ var counter = document.querySelector('.counter');
 var gameover = document.querySelector('.gameover');
 var datetime = "2021-12-01T08:00:00";
 
+var params = new URLSearchParams(window.location.search)
+var mock = params.get("mock") === 'true';
+
 
 function areWeThereYet() {
+  if (mock) {
+    return true;
+  }
+
   return moment().diff(datetime, 'seconds') > 0
 }
 
@@ -24,3 +31,5 @@ setInterval(() => {
     counter.classList.add('hidden');
   }
 }, 1000)
+
+cons
